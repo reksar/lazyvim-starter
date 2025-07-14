63989fe7e956ec4ad81890df7e7b6c8414ffeb84
reksarka <reksarka@gmail.com>
2025-07-14 14:24:49 +0300

    [NEW] Enables standard Vim syntax

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Enable standard Vim syntax in addition to Treesitter.
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.cmd("syntax on")
+    vim.opt.syntax = "on"
+  end,
+})
