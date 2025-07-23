5f8a64e5d232e4f9628b2b01559005f382a546a1
reksarka <reksarka@gmail.com>
2025-07-23 03:00:56 +0300

    [UPD] YAML syntax highlighting
    Disables the Treesitter highlighting for the YAML file type and uses the
    plain Vim syntax instead.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    local disabled_filetypes = { "vim" }
+    -- Disable the Treesitter highlight for the specified file types.
+    -- The plain Vim syntax will be used instead.
+    local disabled_filetypes = { "vim", "yaml" }
     opts.highlight = opts.highlight or {}
     opts.highlight.disable = vim.tbl_extend(
       "force",
