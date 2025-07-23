df9bfd109dd5e69da41dff638542f89396742f42
reksarka <reksarka@gmail.com>
2025-07-23 08:05:58 +0300

    [UPD] Treesitter syn hi groups

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,14 @@ return {
   opts = function(_, opts)
     -- Disable the Treesitter highlight for the specified file types.
     -- The plain Vim syntax will be used instead.
-    local disabled_filetypes = { "vim", "yaml" }
+    local disabled_filetypes = {
+      "bash",
+      "ksh",
+      "posix",
+      "sh",
+      "vim",
+      "yaml",
+    }
     opts.highlight = opts.highlight or {}
     opts.highlight.disable = vim.tbl_extend(
       "force",
